c5cbfc208bd82b966068187d9dedc2d156d691a2
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-01 15:21:46 +0530

    Time: 0 ms (100%), Space: 10.3 MB (63.23%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        
+        reverse(begin(s), end(s));
+        int n = s.length();
+        
+        int i = 0;        
+        int l = 0, r = 0;
+
+
+        while(i < n) {
+            
+            while(i < n && s[i] != ' ') {
+                s[r++] = s[i++];
+            }         
+
+            if(l < r) {
+                reverse(begin(s) + l, begin(s) + r); 
+                // after reversing , then r will assign a space
+                s[r++] = ' ';
+                
+                l = r;
+            }
+
+            i++;
+        }
+        return s.substr(0, r-1);        
+          
+        
+        
+        
+        /*
+
+         - NOT TO BE USED IN THE INTERVIEW
+
         stringstream ss(s);
 
         string token  = "";
@@ -13,5 +46,6 @@ public:
 
         int n = result.size();
         return result.substr(0, n-1);
+        */
     }
 };
