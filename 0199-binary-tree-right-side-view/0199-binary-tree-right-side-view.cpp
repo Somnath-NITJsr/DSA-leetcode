6d452d773a59065e1b0841e773e2419d02de66d3
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-06 09:09:56 +0530

    Time: 0 ms (100%), Space: 14.9 MB (63.95%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
+    void preOrder(TreeNode* root, int level, vector<int>& result) {
+        if(!root) return;
+
+        if(result.size() < level) {
+            result.push_back(root->val);
+        }
+
+        preOrder(root->right, level+1, result);
+        preOrder(root->left, level+1, result);
+    }
     vector<int> rightSideView(TreeNode* root) {
         
-        if(!root) return {};
 
+        // dfs
         vector<int> result;
+        preOrder(root, 1, result);
+
+        /*
+
+        // using the level order traversal
+        
+        if(!root) return {};
 
         queue<TreeNode* > que;
         que.push(root);
@@ -26,6 +43,7 @@ public:
             }
             result.push_back(node->val);
         }
+        */
         return result;
     }
 };
