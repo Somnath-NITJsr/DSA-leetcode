4c27294f17445fde43231072beedc49a2ae3f88d
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-08 09:25:20 +0530

    Time: 0 ms (100%), Space: 13.6 MB (81.57%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
+    int countNodes(TreeNode* root) {
+        if(!root) {
+            return 0;
+        }
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+
+    bool dfs(TreeNode* root, int i, int& totalNode) {
+        if(!root) {
+            return true;
+        }
+
+        if(i > totalNode) {
+            return false;
+        }
+
+        return dfs(root->left, 2*i, totalNode) && dfs(root->right, 2*i + 1, totalNode);
+    }
     bool isCompleteTree(TreeNode* root) {
         
+        int totalNode = countNodes(root);
+
+        int i = 1;
+        return dfs(root, i, totalNode);
+
+
+        /*
+        // USING BFS
         if(!root) return false;
 
         queue<TreeNode* > q;
@@ -26,5 +53,6 @@ public:
             }
         }
         return true;
+        */
     }
 };
