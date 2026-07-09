5ddfcbc9e99bfd7a176c1da85d0069d5fa6b9b75
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-09 10:01:48 +0530

    Time: 17 ms (18.71%), Space: 104.2 MB (25.94%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
+    int count;
     vector<vector<int>> directions {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-    int dfs(vector<vector<int>>& grid, int i, int j, int& m, int& n) {
-        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
-            return 1;
+    void dfs(vector<vector<int>>& grid, int i, int j, int& m, int& n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            count++;
+            return;
         }
-
-        if(grid[i][j] == -1) {
-            return 0;
+        
+        if (grid[i][j] == -1) {
+            return;
         }
 
         grid[i][j] = -1; // marked visited
 
-        return dfs(grid, i, j+1, m, n) + dfs(grid, i, j-1, m, n) + dfs(grid, i+1, j, m, n) + dfs(grid, i-1, j,m ,n);
+        dfs(grid, i, j+1, m, n);
+        dfs(grid, i, j-1, m, n);
+        dfs(grid, i+1, j, m, n);
+        dfs(grid, i-1, j,m ,n);
     }
 
     int bfs(vector<vector<int>>& grid, int i, int j, int& m, int& n) {
@@ -49,15 +54,16 @@ public:
     int islandPerimeter(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-
+        count = 0;
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(grid[i][j] == 1) {
-                    // return dfs(grid, i, j, m, n);
-                    return bfs(grid, i, j, m, n);
+                    dfs(grid, i, j, m, n);
+                    return count;
+                    // return bfs(grid, i, j, m, n);
                 }
             }
         }
-        return 0;
+        return -1;
     }
 };
