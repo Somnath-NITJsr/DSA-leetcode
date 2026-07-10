089579b3f1399a8bac9919ae81c1d81d0a54f505
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-10 12:27:16 +0530

    Time: 0 ms (100%), Space: 16.8 MB (5.05%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void inOrder(TreeNode* root, string& s) {
+        if(!root) {
+            return;
+        }
+
+        // leaf node
+        if(!root->left && !root->right) {
+            s += to_string(root->val) + "#";
+            return;
+        }
+
+        inOrder(root->left, s);
+        inOrder(root->right, s);
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        string s1 = "";
+        string s2 = "";
+
+        inOrder(root1, s1);
+        inOrder(root2, s2);
+
+        return s1 == s2;
+    }
+};
