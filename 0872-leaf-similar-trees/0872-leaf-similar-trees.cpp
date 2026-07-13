37f757c29a17cc41663655e836679bb6c7e4901f
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-13 10:44:47 +0530

    Time: 0 ms (100%), Space: 16.6 MB (7.19%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -1,27 +1,26 @@
 class Solution {
-public:
-    void inOrder(TreeNode* root, string& s) {
+public: 
+    void traverseTree(TreeNode* root, string& s) {
         if(!root) {
             return;
         }
 
-        // leaf node
         if(!root->left && !root->right) {
             s += to_string(root->val) + "#";
             return;
         }
 
-        inOrder(root->left, s);
-        inOrder(root->right, s);
+        traverseTree(root->left, s);
+        traverseTree(root->right, s);
+        return;
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-
-        // to optimise the space, we used the string, otherwise vector can be used
+        // we can use vector , but using the string optimises the space.
         string s1 = "";
         string s2 = "";
 
-        inOrder(root1, s1);
-        inOrder(root2, s2);
+        traverseTree(root1, s1);
+        traverseTree(root2, s2);
 
         return s1 == s2;
     }
