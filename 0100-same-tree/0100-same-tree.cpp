4017c47e3477cadbf1e27da9c6f4054bb7b8ba5f
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-14 18:16:45 +0530

    Time: 0 ms (100%), Space: 12.6 MB (97.05%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
-    void solve(TreeNode* root, vector<string>& tree, bool isLeft) {       
-        if(!root) {
-            if(isLeft) {
-            tree.push_back("NULL_");
-            }
-            else{
-                tree.push_back("_NULL");
-            }
-            return;
+    // void solve(TreeNode* root, vector<string>& tree, bool isLeft) {       
+    //     if(!root) {
+    //         if(isLeft) {
+    //         tree.push_back("NULL_");
+    //         }
+    //         else{
+    //             tree.push_back("_NULL");
+    //         }
+    //         return;
+    //     }
+
+    //     tree.push_back(to_string(root->val));
+    //     solve(root->left, tree, true);
+    //     solve(root->right, tree, false);
+    // }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // vector<string> tree1;
+        // vector<string> tree2;
+
+        // solve(p, tree1, true);
+        // solve(q, tree2, true);
+
+        // return tree1 == tree2;
+
+        if(!p && !q) {
+            return true; // no nodes
         }
 
-        tree.push_back(to_string(root->val));
-        solve(root->left, tree, true);
-        solve(root->right, tree, false);
-    }
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<string> tree1;
-        vector<string> tree2;
+        if(!p || !q) {
+            return false; // either p or q exists
+        }
 
-        solve(p, tree1, true);
-        solve(q, tree2, true);
+        if(p->val != q->val) {
+            return false;
+        }
 
-        return tree1 == tree2;
+        // check whether left of p is same as left of q
+        // similarly check whether the right of q is same as right of q
+        return isSameTree(p->left, q->left)
+                && isSameTree(p->right, q->right);
     }
 };
