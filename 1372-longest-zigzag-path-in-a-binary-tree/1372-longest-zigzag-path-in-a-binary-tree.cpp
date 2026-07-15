308607aa6eede3f2e04f306696fd90398c0c937e
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-15 19:44:20 +0530

    Time: 459 ms (19.32%), Space: 102.3 MB (8.56%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
-    void solve(TreeNode* root, int& maxPath, int steps, bool goLeft) {
+    // void solve(TreeNode* root, int& maxPath, int steps, bool goLeft) {
+    //     if(!root) {
+    //         return;
+    //     }
+
+    //     maxPath = max(maxPath, steps);
+
+    //     if(goLeft) {
+    //         solve(root->left, maxPath, steps+1, false);
+    //         solve(root->right, maxPath, 1, true);
+    //     } else {
+    //         solve(root->right, maxPath, steps+1, true);
+    //         solve(root->left, maxPath, 1, false);
+    //     }
+
+    // }
+
+    void solve(TreeNode* root, int l, int r, int& maxPath) {
         if(!root) {
             return;
         }
 
-        maxPath = max(maxPath, steps);
+        maxPath = max({maxPath, l, r});
 
-        if(goLeft) {
-            solve(root->left, maxPath, steps+1, false);
-            solve(root->right, maxPath, 1, true);
-        } else {
-            solve(root->right, maxPath, steps+1, true);
-            solve(root->left, maxPath, 1, false);
-        }
-
+        solve(root->left, r+1, 0, maxPath);
+        solve(root->right, 0, l+1, maxPath);
     }
     int longestZigZag(TreeNode* root) {
-        int maxPath = 0;
-        int steps   = 0;
-        // bool goLeft = true;
+        // int maxPath = 0;
+        // int steps   = 0;
+        // // bool goLeft = true;
 
-        solve(root, maxPath, steps, true);
-        solve(root, maxPath, steps, false);
+        // solve(root, maxPath, steps, true);
+        // solve(root, maxPath, steps, false);
+        // return maxPath;
+
+        int maxPath = 0;
+        solve(root, 0, 0, maxPath);
         return maxPath;
     }
 };
