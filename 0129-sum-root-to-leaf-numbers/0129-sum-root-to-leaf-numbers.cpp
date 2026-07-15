1171e3e5c44b2b9cedc7f4fa15b4fa67be52187e
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-15 11:21:29 +0530

    Time: 0 ms (100%), Space: 11.9 MB (39.12%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    int findSum(TreeNode* root, string s) {
+    // int findSum(TreeNode* root, string s) {
+    //     if(!root) {
+    //         return 0;
+    //     }
+
+    //     s += to_string(root->val);
+
+    //     if(!root->left && !root->right) {
+    //         return stoi(s);
+    //     }
+
+    //     return findSum(root->left, s)+ findSum(root->right, s);
+    // }
+
+    int findSUM(TreeNode* root, int sum) {
         if(!root) {
             return 0;
         }
 
-        s += to_string(root->val);
+        // sum = 0 , 10* sum + root->val
+        sum = (10* sum) + root->val;
 
         if(!root->left && !root->right) {
-            return stoi(s);
+            return sum;
         }
 
-        return findSum(root->left, s)+ findSum(root->right, s);
+        return findSUM(root->left, sum) + findSUM(root->right, sum);
     }
+
     int sumNumbers(TreeNode* root) {
         if(!root) {
             return 0;
         }
 
-        string s = "";
+        return findSUM(root, 0);
 
-        return findSum(root, s);
+        // string s = "";
+
+        // return findSum(root, s);
         
     }
 };
