61357da8d68062ee36c9e9c28a097fbef270bbcf
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-17 19:02:19 +0530

    Time: 13 ms (67.16%), Space: 37 MB (42.97%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -5,6 +5,29 @@ public:
         int n          = grid.size();
         int countPairs = 0;
 
+        map<vector<int>, int> mp;
+        // 1
+        for(int row=0; row<n; row++) {
+            
+            // passes the entire vector [3, 2, 1] to the map.
+            mp[grid[row]]++;
+
+        }
+
+        // 2
+        for(int col=0; col<n; col++) {
+            
+            vector<int> temp;
+            for(int row=0; row<n; row++) {
+                temp.push_back(grid[row][col]);
+            }
+            // the freq will be added to the countPairs
+            countPairs += mp[temp];
+        }
+
+        return countPairs;
+        /*
+
         for(int row=0; row<n; row++) {
             for(int col=0; col<n; col++) {
                 bool isEqual = true;
@@ -23,5 +46,6 @@ public:
             }
         }
         return countPairs;
+        */
     }
 };
