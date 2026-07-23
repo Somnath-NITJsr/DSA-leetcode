fdf0396c262e9ffe669a90c959711b7260f053dc
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-23 17:24:10 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -19,11 +19,27 @@ public:
         return t[n] = one_step + two_step;
     }
     int climbStairs(int n) {
-        
-        int t[46];
-        memset(t, -1, sizeof(t));
 
-        return solve(n, t);
+        if(n == 0 || n == 1 || n == 2) {
+            return n;
+        }
+
+        vector<int> arr(n+1);
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+
+        // now calculate after i = 2
+        for(int i=3; i<=n; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        return arr[n];
+        
+        // int t[46];
+        // memset(t, -1, sizeof(t));
+
+        // return solve(n, t);
 
     }
 };
