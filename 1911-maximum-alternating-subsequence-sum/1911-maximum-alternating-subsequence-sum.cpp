f5c99c4532e4925de85c37bcb4a815be0866a97c
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-30 10:27:54 +0530

    Time: 265 ms (41.48%), Space: 157.1 MB (50.4%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -23,9 +23,25 @@ public:
 
     }
     long long maxAlternatingSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<long long >> t (100000, vector<long long >(2, -1));
 
-        return solve(nums, 0, n, t, true); // flag is true for even index
+        int n = nums.size();
+
+        vector<vector<long>> t(n+1, vector<long>(2, 0));
+
+        for(int i=1; i<n+1; i++) {
+
+            // even +, -, +, -
+            t[i][0] = max(t[i-1][1] - nums[i-1], t[i-1][0]);
+
+            // odd   take                          skip
+            t[i][1] = max(t[i-1][0] + nums[i-1], t[i-1][1]);
+        }
+
+        return max(t[n][0], t[n][1]);
+
+        // int n = nums.size();
+        // vector<vector<long long >> t (100000, vector<long long >(2, -1));
+
+        // return solve(nums, 0, n, t, true); // flag is true for even index
     }
 };
