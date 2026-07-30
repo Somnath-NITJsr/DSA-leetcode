955441d56b2962f8c5dd4e5c57f46ca1cb34017b
Somnath Rooj <somnathnitpatna@gmail.com>
2026-07-30 17:58:06 +0530

    Time: 76 ms (48.31%), Space: 14.4 MB (61.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -24,9 +24,30 @@ public:
         return max(take, skip);
     }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();    
-        vector<vector<int>> t(n+1, vector<int>(n+1, -1));
 
-        return solve(nums, 0, -1, t, n);
+        int n = nums.size();
+
+        vector<int> t(n, 1);
+
+        int maxLIS = 1;
+
+        for(int i=0; i<n; i++) {
+
+            for(int j=0; j<i; j++) {
+
+                if(nums[j] < nums[i]) {
+
+                    t[i]   = max(t[i], t[j]+1);
+                    maxLIS = max(maxLIS, t[i]);
+                }
+            }
+        }
+
+        return maxLIS;
+        
+        // int n = nums.size();    
+        // vector<vector<int>> t(n+1, vector<int>(n+1, -1));
+
+        // return solve(nums, 0, -1, t, n);
     }
 };
