614301242c1ef439e763016ee7b9829579125b1d
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-01 21:16:27 +0530

    Time: 27 ms (57.05%), Space: 27.5 MB (55.39%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -22,8 +22,35 @@ public:
         int m = text1.size();
         int n = text2.size();
 
-        vector<vector<int>> t(m+1, vector<int>(n+1, -1));
 
-        return findLCS(text1, text2, 0, 0, m, n, t);
+        vector<vector<int>> t(m+1, vector<int>(n+1, 0));
+
+        // for the row
+        for(int row=0; row<m+1; row++) {
+            t[row][0] = 0;
+        }
+
+        // for the col
+        for(int col=0; col<n+1; col++) {
+            t[0][col] = 0;
+        }
+
+        for(int i=1; i<m+1; i++) {
+
+            for(int j=1; j<n+1; j++) {
+
+                if(text1[i-1] == text2[j-1]) {
+                    t[i][j] = 1 + t[i-1][j-1];
+                } else {
+                    t[i][j] = max(t[i-1][j], t[i][j-1]);
+                }
+            }
+        }
+
+        return t[m][n];
+
+        // vector<vector<int>> t(m+1, vector<int>(n+1, -1));
+
+        // return findLCS(text1, text2, 0, 0, m, n, t);
     }
 };
