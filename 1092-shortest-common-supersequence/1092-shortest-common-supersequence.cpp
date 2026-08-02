7803cbbb483b4f8a52438476b06b24116fff540a
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-02 19:35:57 +0530

    Time: 29 ms (13.05%), Space: 26.8 MB (43.51%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -6,26 +6,28 @@ public:
 
         vector<vector<int>> t(m+1, vector<int>(n+1, 0));
 
-        // for the row
-        for(int row=0; row<m+1; row++) {
-            t[row][0] = 0;
-        }
+        // // for the row
+        // for(int row=0; row<m+1; row++) {
+        //     t[row][0] = 0;
+        // }
 
-        // for the col
-        for(int col=0; col<n+1; col++) {
-            t[0][col] = 0;
-        }
+        // // for the col
+        // for(int col=0; col<n+1; col++) {
+        //     t[0][col] = 0;
+        // }
 
         // build the table
 
-        for(int i=1; i<m+1; i++) {
+        for(int i=0; i<m+1; i++) {
 
-            for(int j=1; j<n+1; j++) {
+            for(int j=0; j<n+1; j++) {
 
-                if(s1[i-1] == s2[j-1]) {
+                if(i == 0 || j == 0) {
+                    t[i][j] = i+j;
+                } else if(s1[i-1] == s2[j-1]) {
                     t[i][j] = 1 + t[i-1][j-1];
                 } else {
-                    t[i][j] = max(t[i-1][j], t[i][j-1]);
+                    t[i][j] = 1 + min(t[i-1][j], t[i][j-1]);
                 }
             }
         }
@@ -40,11 +42,11 @@ public:
                 ans.push_back(s1[i-1]);
                 i--;
                 j--;
-            } else if(t[i-1][j] > t[i][j-1]) {
+            } else if(t[i-1][j] < t[i][j-1]) { // if not same , then we move towards the min part -- we take the ith part
                 ans.push_back(s1[i-1]);
                 i--;
             } else {
-                ans.push_back(s2[j-1]);
+                ans.push_back(s2[j-1]); // we take the jth part, as we move towards the min
                 j--;
             }
         }
