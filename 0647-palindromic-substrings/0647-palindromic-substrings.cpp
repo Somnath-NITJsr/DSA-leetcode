c0648b163070fc5efc60b41dbcc0fde1b0138330
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-03 17:46:03 +0530

    Time: 1284 ms (7.77%), Space: 34.7 MB (12.05%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    bool checkPal(const string& s, int i, int j) {
-
-        while(i <= j) {
-
-            if(s[i] != s[j]) {
-                return false;
-            }
-
-            i++;
-            j--;
+    bool checkPal(const string& s, int i, int j, vector<vector<int>>& t) {
+        if(i > j) {
+            return true;
         }
-        return true;
+
+        if(t[i][j] != -1) {
+            return t[i][j]; // -1 _ not solved, 0 _ false, 1 _ true
+        }
+
+        if(s[i] == s[j]) {
+
+            return checkPal(s, i+1, j-1, t);
+        }
+
+        return false;
     }
     int countSubstrings(string s) {
         int n = s.length();
 
+
         int count = 0;
+        vector<vector<int>> t(n+1, vector<int>(n+1, -1));
 
         for(int i=0; i<n; i++) {
-            
+
             for(int j=i; j<n; j++) {
 
-                if(checkPal(s, i, j)) {
+                if(checkPal(s, i, j, t)) {
                     count++;
                 }
             }
