a5325c21de6005bbc873bfe2091acc2b7bf2063d
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-03 18:19:49 +0530

    Time: 32 ms (28.62%), Space: 11 MB (49.92%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -19,20 +19,45 @@ public:
     int countSubstrings(string s) {
         int n = s.length();
 
+        vector<vector<bool>> t(n, vector<bool>(n, false));
 
         int count = 0;
-        vector<vector<int>> t(n+1, vector<int>(n+1, -1));
 
-        for(int i=0; i<n; i++) {
+        for(int L=1; L<=n; L++) {
 
-            for(int j=i; j<n; j++) {
+            for(int i=0; i+L-1<n; i++) {
 
-                if(checkPal(s, i, j, t)) {
+                int j = i+L-1;
+
+                if(i == j) {
+                    t[i][i] = true;
+                } else if(i+1 == j) {
+                    t[i][j] = (s[i] == s[j]);
+                } else {
+                    t[i][j] = (s[i] == s[j] && t[i+1][j-1]); // if both are true, then true
+                }
+
+                if(t[i][j] == true) {
                     count++;
                 }
             }
         }
 
         return count;
+
+        // int count = 0;
+        // vector<vector<int>> t(n+1, vector<int>(n+1, -1));
+
+        // for(int i=0; i<n; i++) {
+
+        //     for(int j=i; j<n; j++) {
+
+        //         if(checkPal(s, i, j, t)) {
+        //             count++;
+        //         }
+        //     }
+        // }
+
+        // return count;
     }
 };
