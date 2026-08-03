0a31e22d6b43dc242bb60990acfaae23dbf76a7c
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-03 16:26:15 +0530

    Time: 7 ms (60.96%), Space: 13.3 MB (31.51%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -24,8 +24,24 @@ public:
         int m = word1.size();
         int n = word2.size();
 
-        vector<vector<int>> t(m+1, vector<int>(n+1, -1));
+        vector<vector<int>> t(m+1, vector<int>(n+1, 0));
 
-        return solve(word1, word2, m, n, t);
+        for(int i=0; i<m+1; i++) {
+
+            for(int j=0; j<n+1; j++) {
+
+                if(i == 0 || j == 0) {
+                    t[i][j] = i+j;
+                } else if(word1[i-1] == word2[j-1]) {
+                    t[i][j] = t[i-1][j-1];
+                } else {
+                    t[i][j] = 1 + min({t[i][j-1], t[i-1][j], t[i-1][j-1]});
+                }
+            }
+        }
+
+        return t[m][n];
+        // vector<vector<int>> t(m+1, vector<int>(n+1, -1));
+        // return solve(word1, word2, m, n, t);
     }
 };
