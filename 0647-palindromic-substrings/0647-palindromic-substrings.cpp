afdcbdca8ac605e9e262c724334bc2b267fb224e
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-04 08:57:52 +0530

    Time: 31 ms (31.1%), Space: 11 MB (46.62%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -18,46 +18,48 @@ public:
     }
     int countSubstrings(string s) {
         int n = s.length();
-        int count = 0;
+        // int count = 0;
         // Capturing [&] lets you use s and n directly:
-        auto checkPal = [&](int i, int j) {
+        // auto checkPal = [&](int i, int j) {
 
-            while(i >= 0 && j < n && s[i] == s[j]) {
-                count++;
-                i--;
-                j++;
-            }
+        //     while(i >= 0 && j < n && s[i] == s[j]) {
+        //         count++;
+        //         i--;
+        //         j++;
+        //     }
 
-        };
+        // };
 
-        for(int i=0; i<n; i++) {
-            // we don't need to pass the s, i as we are using the [&]..
-            checkPal(i, i);
-            checkPal(i, i+1);
-        }
-
-
-        return count;
-        /* 
-         BLUE PRINT
+        // for(int i=0; i<n; i++) {
+        //     // we don't need to pass the s, i as we are using the [&]..
+        //     checkPal(i, i);
+        //     checkPal(i, i+1);
+        // }
 
 
-        vector<vector<bool>> t(n, vector<bool>(n, false));
+        // return count;
+        
+        // BLUE PRINT
+
         int count = 0;
+        vector<vector<bool>> t(n, vector<bool>(n, false));
+
         for(int L=1; L<=n; L++) {
 
             for(int i=0; i+L-1<n; i++) {
 
-                int j = i+L-1;
+                int j = i+L-1; // i = 0 , L = 2 , j = 2 - 1 + 0 = 1
 
-                if(i == j) {
+                if(i == j) { // diagonal entries are always true, single element
                     t[i][i] = true;
-                } else if(i+1 == j) {
-                    t[i][j] = (s[i] == s[j]);
-                } else {
-                    t[i][j] = (s[i] == s[j] && t[i+1][j-1]); // if both are true, then true
+                } else if(i+1 == j) { // length = 2, if palindrome then true
+                    t[i][j] = (s[i] == s[j]); 
+                } else { // length > 2
+                    t[i][j] = (s[i] == s[j]) && t[i+1][j-1]; 
+                    // check the i, j and i+1, j-1 part
                 }
 
+                // after all if its true, then the substring is a palindrome
                 if(t[i][j] == true) {
                     count++;
                 }
@@ -66,8 +68,7 @@ public:
 
         return count;
 
-        */
-
+        // for the REC + MEMO;
         // int count = 0;
         // vector<vector<int>> t(n+1, vector<int>(n+1, -1));
 
