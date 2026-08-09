99ee218b50cbba60aa76ed0b961985baa5b1c81e
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-09 17:47:08 +0530

    4 ms (77.27%) 128.4 MB (54.48%)

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,31 +1,52 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> pos;
-        vector<int> neg;
 
+        int n = nums.size();
 
-        for(int i=0; i<nums.size(); i++) {
-
-            if(nums[i] < 0) {
-                neg.push_back(nums[i]);
-            } else {
-                pos.push_back(nums[i]);
-            }
-        }
+        vector<int> result(n, 0);
 
         int pPos = 0;
         int pNeg = 1;
 
-        for(int i=0; i<pos.size(); i++) {
-            nums[pPos] = pos[i];
-            nums[pNeg] = neg[i];
+        for(int i=0; i<n; i++) {
 
-            pPos += 2;
-            pNeg += 2;
+            if(nums[i] > 0) {
+                result[pPos] = nums[i];
+                pPos         += 2;
+            } else {
+                result[pNeg] = nums[i];
+                pNeg         += 2;
+            }
         }
 
-        return nums;
+        return result;
+
+        // vector<int> pos;
+        // vector<int> neg;
+
+
+        // for(int i=0; i<nums.size(); i++) {
+
+        //     if(nums[i] < 0) {
+        //         neg.push_back(nums[i]);
+        //     } else {
+        //         pos.push_back(nums[i]);
+        //     }
+        // }
+
+        // int pPos = 0;
+        // int pNeg = 1;
+
+        // for(int i=0; i<pos.size(); i++) {
+        //     nums[pPos] = pos[i];
+        //     nums[pNeg] = neg[i];
+
+        //     pPos += 2;
+        //     pNeg += 2;
+        // }
+
+        // return nums;
 
     }
 };
