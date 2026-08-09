4c1efdfc00574c62705eebb6486397b81c18953f
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-09 09:18:11 +0530

    0 ms (100%) 28.3 MB (41.88%)

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,22 +3,46 @@ public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
 
-        unordered_map<int, int> mpp;
 
-        for(auto& it: nums) {
+        // Assumption
 
-            mpp[it]++;
+        int candidate = nums[0];
+        int count     = 1;
 
-        }
 
-        for(auto& it: mpp) {
+        for(int i=1; i<n; i++) {
 
-            if(it.second > (n/2)) {
-                return it.first;
+            if(candidate == nums[i]) {
+                count++;
+            } else {
+                count--;
             }
-            
+
+            if(count == 0) {
+                candidate = nums[i];
+                count++;
+            }
         }
+
+        // Verification not required, in Majority Element I - max 1 majority element
+        return candidate;
+
+        // unordered_map<int, int> mpp;
+
+        // for(auto& it: nums) {
+
+        //     mpp[it]++;
+
+        // }
+
+        // for(auto& it: mpp) {
+
+        //     if(it.second > (n/2)) {
+        //         return it.first;
+        //     }
+            
+        // }
         
-        return -1;
+        // return -1;
     }
 };
