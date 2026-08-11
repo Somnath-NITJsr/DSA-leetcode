2a10822f3a1ad5bdc774dc0b33a0a1a918bd1351
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-11 10:38:24 +0530

    5 ms (54.39%) 65.1 MB (42.2%)

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,22 +2,49 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         
-        int n = nums.size();
+        // using the slow and fast pointer - same as detect cycle in LL
 
-        unordered_map<int, int> mp;
+        int slow = nums[0];
+        int fast = nums[0]; 
 
-        for(auto& it: nums) {
-            
-            mp[it]++;
+        slow = nums[slow]; // move 1
+        fast = nums[nums[fast]]; // move 2
+
+        while(slow != fast) {
+
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        // now they meet
+
+        slow = nums[0];
+        // now they will move 1 steps
+
+        while(slow != fast) {
+
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        for(auto& it: mp) {
-            
-            if(it.second > 1) {
-                return it.first;
-            }
-        }
+        // again they had met, return slow/ fast.. this is the duplicate
 
-        return -1;
+        return fast;
+
+
+        // unordered_map<int, int> mp;
+
+        // for(auto& it: nums) {
+            
+        //     mp[it]++;
+        // }
+
+        // for(auto& it: mp) {
+            
+        //     if(it.second > 1) {
+        //         return it.first;
+        //     }
+        // }
+
+        // return -1;
     }
 };
