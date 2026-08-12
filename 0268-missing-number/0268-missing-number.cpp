e9f598578d244bf6c1373b432f9e6de710aca067
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-12 10:11:26 +0530

    0 ms (100%) 21.7 MB (71.47%)

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        sort(begin(nums), end(nums));
+
+        for(int i=0; i<n; i++) {
+
+            if(nums[i] != i) {
+                return i;
+            }
+        }
+        
+        return n;
+    }
+};
