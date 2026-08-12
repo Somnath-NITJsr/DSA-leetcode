16d48b354bdca0a6b3c9e66f3f7881689c5f12bc
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-12 10:21:53 +0530

    0 ms (100%) 21.9 MB (39.34%)

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,13 +3,29 @@ public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
 
-        long long sum  = accumulate(begin(nums), end(nums), 0LL);
+        // Approach 3
 
-        long long nSum = n*(n+1)/2;
+        int result = n;
 
-        return nSum - sum;
+        for(int i=0; i<n; i++) {
+            
+            result = result ^ i;
+            result = result ^ nums[i];
+        }
+
+        return result;
 
 
+        // Approach 2
+        // long long sum  = accumulate(begin(nums), end(nums), 0LL);
+
+        // long long nSum = n*(n+1)/2;
+
+        // return nSum - sum;
+
+
+
+        // Approach 1
         // sort(begin(nums), end(nums));
 
         // for(int i=0; i<n; i++) {
