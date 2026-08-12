d8fa9e56ac3fa9c2506e68803ce156363176b155
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-12 09:38:38 +0530

    0 ms (100%) 12.5 MB (12.64%)

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0;
-        int j = 0;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
-        vector<int> temp;
+        while(i >= 0 && j >= 0) {
 
-        while(i < m && j < n) {
-
-            if(nums1[i] <= nums2[j]) {
-                temp.push_back(nums1[i]);
-                i++;
+            if(nums1[i] >= nums2[j]) {
+                nums1[k--] = nums1[i--];
             } else {
-                temp.push_back(nums2[j]);
-                j++;
+                nums1[k--] = nums2[j--];
             }
         }
 
-        while(i < m) {
-            temp.push_back(nums1[i++]);
+        while(j >= 0) {
+
+            nums1[k--] = nums2[j--];
         }
 
-        while(j < n) {
-            temp.push_back(nums2[j++]);
-        }
 
-        nums1 = temp;
+        return;
+
+
+
+        // vector<int> temp;
+
+        // while(i < m && j < n) {
+
+        //     if(nums1[i] <= nums2[j]) {
+        //         temp.push_back(nums1[i]);
+        //         i++;
+        //     } else {
+        //         temp.push_back(nums2[j]);
+        //         j++;
+        //     }
+        // }
+
+        // while(i < m) {
+        //     temp.push_back(nums1[i++]);
+        // }
+
+        // while(j < n) {
+        //     temp.push_back(nums2[j++]);
+        // }
+
+        // nums1 = temp;
     }
 };
