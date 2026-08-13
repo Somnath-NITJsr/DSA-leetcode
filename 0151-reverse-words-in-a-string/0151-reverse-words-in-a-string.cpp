f2f78821aa05e4d0167764865a6fc334564a398c
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-13 17:35:39 +0530

    0 ms (100%) 10.1 MB (79.44%)

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,9 +2,38 @@ class Solution {
 public:
     string reverseWords(string s) {
         
+        int n = s.length();
+
+        // 1. reverse the string
+        reverse(begin(s), end(s));
+
+        // 2. l, r, i
+
+        int l = 0;
+        int r = 0;
+        int i = 0;
+
+        while(i < n) {
+
+            while(i < n && s[i] != ' ') {
+                s[r++] = s[i++];
+            }
 
 
-        // tokeniser
+            // reverse
+            if(l < r) {
+                reverse(s.begin() + l, s.begin() + r);
+                s[r] = ' ';
+                r++;
+                l    = r;
+            }
+            i++;
+        }
+
+        return s.substr(0, r-1);
+
+        /*
+            // tokeniser
         stringstream ss(s);
 
         string token  = "";
@@ -17,5 +46,7 @@ public:
         int n = result.size();
 
         return result.substr(0, n-1);
+
+        */
     }
 };
