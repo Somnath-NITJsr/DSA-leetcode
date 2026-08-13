605f06f2438795f17f48124d9f6cfbf2c381789d
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-13 17:15:39 +0530

    10 ms (9.78%) 23.8 MB (8.22%)

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,38 +2,9 @@ class Solution {
 public:
     string reverseWords(string s) {
         
-        reverse(begin(s), end(s));
-        int n = s.length();
-        
-        int i = 0;        
-        int l = 0, r = 0;
 
 
-        while(i < n) {
-            
-            while(i < n && s[i] != ' ') {
-                s[r++] = s[i++];
-            }         
-
-            if(l < r) {
-                reverse(begin(s) + l, begin(s) + r); 
-                // after reversing , then r will assign a space
-                s[r++] = ' ';
-                
-                l = r;
-            }
-
-            i++;
-        }
-        return s.substr(0, r-1);        
-          
-        
-        
-        
-        /*
-
-         - NOT TO BE USED IN THE INTERVIEW
-
+        // tokeniser
         stringstream ss(s);
 
         string token  = "";
@@ -43,9 +14,8 @@ public:
 
             result = token + " " + result;
         }
-
         int n = result.size();
+
         return result.substr(0, n-1);
-        */
     }
 };
