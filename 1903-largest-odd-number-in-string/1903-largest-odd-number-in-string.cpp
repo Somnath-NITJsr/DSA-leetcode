245a91cbc0ab6d9ada830b9c4acf78108620923d
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-13 17:54:19 +0530

    4 ms (22.26%) 17.7 MB (85.65%)

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n =  num.length();
+        int i = n - 1;;
+        for(; i >= 0; i--) {
+
+            if(num[i] % 2 != 0) {
+                break;
+            }
+        }
+        
+        return num.substr(0, i+1);
+    }
+};
