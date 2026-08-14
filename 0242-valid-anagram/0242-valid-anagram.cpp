9f3faa720b34dc226d1b34b7a6bd78b3e0b00fad
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-14 10:40:49 +0530

    327 ms (6.77%) 325.7 MB (11.54%)

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+
+        sort(begin(s), end(s));
+        sort(begin(t), end(t));
+
+
+        if(m != n) {
+            return false;
+        }
+
+        string temp = s + t;
+
+        for(int i=0; i<n; i++) {
+
+            string check = temp.substr(i, n);
+
+            if(check == t) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
