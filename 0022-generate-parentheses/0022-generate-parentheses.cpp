26d7d807eaddbdde438ec23b335e5b1e5aaee1dd
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-17 14:58:11 +0530

    0 ms (100%) 12.9 MB (89.87%)

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -37,11 +37,32 @@ public:
         solve(curr, n, result);
         curr.pop_back();
     }
+
+    void genParenthesis(string& curr,int open, int close, int n, vector<string>& result) {
+
+        if(open + close == 2*n) {
+            result.push_back(curr);
+            return;
+        }
+
+        if(open < n) {
+            curr.push_back('(');
+            genParenthesis(curr, open + 1, close, n, result);
+            curr.pop_back();
+        } 
+
+        if(close < open) {
+            curr.push_back(')');
+            genParenthesis(curr, open, close + 1, n, result);
+            curr.pop_back();
+        }
+    }
     vector<string> generateParenthesis(int n) {
         vector<string> result;
         string curr = "";
 
-        solve(curr, n, result);
+        // solve(curr, n, result);
+        genParenthesis(curr, 0, 0, n, result);
         return result;
     }
 };
