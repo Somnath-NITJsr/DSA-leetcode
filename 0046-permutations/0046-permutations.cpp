1f3ee409bd7e82a4185f0525f137d9b24793d78a
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-18 10:06:43 +0530

    0 ms (100%) 10.5 MB (89.09%)

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -21,13 +21,36 @@ public:
             }
         }
     }
+
+    void getPermute(vector<int>& nums, int idx, int& n, vector<vector<int>>& result) {
+
+        if(idx == n - 1) {
+            result.push_back(nums);
+            return;
+        }
+
+
+        for(int i = idx; i < n; i++) {
+
+            swap(nums[i], nums[idx]);
+
+            getPermute(nums, idx + 1, n, result);
+
+            swap(nums[i], nums[idx]);
+        }
+    }
+
     vector<vector<int>> permute(vector<int>& nums) {
+
         int n = nums.size();
+
         unordered_set<int> st;
         vector<int> temp;
         vector<vector<int>> result;
 
-        solve(nums, n, temp, st, result);
+        // solve(nums, n, temp, st, result);
+
+        getPermute(nums, 0, n, result);
 
         return result;
     }
