5150017303cd482dd45d8baec40a74f8e4160102
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-25 22:35:37 +0530

    11 ms (91.24%) 45.8 MB (71.41%)

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+
+        sort(begin(people), end(people));
+
+
+        int i = 0;
+        int j = n - 1;
+
+        int boat = 0;
+        while(i <= j) {
+
+            if(people[i] + people[j] <= limit) {
+                i++;
+                j--;
+            } else {
+                j--;
+            }
+            boat++;
+        }
+
+        return boat;
+    }
+};
