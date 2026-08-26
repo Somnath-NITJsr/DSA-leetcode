7a0ef38eb04ad6c05c97ffe79b1033d56c5799ae
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-26 22:58:33 +0530

    0 ms (100%) 7.9 MB (49.05%)

--- 0991-broken-calculator/0991-broken-calculator.cpp
+++ 0991-broken-calculator/0991-broken-calculator.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int brokenCalc(int startValue, int target) {
+        
+        // we just reversed the logic, we are going from the target to startValue
+        // instead of -1 , we are doing +1 &&  *2 to /2
+        
         if(startValue >= target) {
             return startValue - target;
         }
