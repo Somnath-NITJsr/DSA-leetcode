16e6aa821db51d5a5cbeeefe6217e39535bdf288
Somnath Rooj <somnathnitpatna@gmail.com>
2026-08-27 22:53:03 +0530

    0 ms (100%) 8.5 MB (38.53%)

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -7,14 +7,19 @@ public:
             return "";
         }
 
+
+        // n/2 because, the first half will be the mirror image of the rest
         for(int i=0; i<n/2; i++) {
 
             if(palindrome[i] != 'a') {
+                // change the first letter to a, to make a non palindrome
+                // lexiographically
                 palindrome[i] = 'a';
                 return palindrome;
             }
         }
 
+        // if all a a a a a, then change the last letter to b, to maintain the lexiographicall order
         palindrome[n-1] = 'b';
         return palindrome;
     }
