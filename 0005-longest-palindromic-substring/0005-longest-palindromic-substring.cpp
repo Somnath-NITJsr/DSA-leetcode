d5381594d583ab2f84d6f9bc1b07be818944a2cf
Somnath Rooj <somnathnitpatna@gmail.com>
2026-09-06 23:16:54 +0530

    339 ms (14.21%) 26.1 MB (23.31%)

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -5,31 +5,30 @@ public:
 
         vector<vector<bool>> t(n, vector<bool>(n, false));
 
-        string ans = "";
         int maxLen = 0;
-        for(int L=1; L<=n; L++) {
+        int index = 0;
+        for (int L = 1; L <= n; L++) {
 
-            for(int i=0; i+L-1<n; i++) {
+            for (int i = 0; i + L - 1 < n; i++) {
 
-                int j      = i+L-1;
+                int j = i + L - 1;
 
-                if(i == j) {
+                if (i == j) {
                     t[i][i] = true;
-                } else if(i+1 == j) {
+                } else if (i + 1 == j) {
                     t[i][j] = (s[i] == s[j]);
                 } else {
-                    t[i][j] = (s[i] == s[j]) && t[i+1][j-1];
+                    t[i][j] = (s[i] == s[j]) && t[i + 1][j - 1];
                 }
 
-                if(t[i][j] == true) {
-                    if(L > maxLen) {
+                if (t[i][j] == true) {
+                    if (L > maxLen) {
                         maxLen = L;
-                        ans = s.substr(i, L);
+                        index = i;
                     }
                 }
             }
         }
-        return ans;
-        
+        return s.substr(index, maxLen);
     }
 };
